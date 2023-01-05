7711d1e9165f2d7d4b37b5aa130e358bf8d439a1
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2023-01-05 11:22:31 +0100

    added the url and packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import React from "react";
 import "./App.css";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  // Marker,
+  // Annotation,
+  ZoombleGroup,
+} from "react-simple-maps";
+// import ReactTooltip from "react-tooltip";
+// const markers = [
+//   {
+//     markerOffset: -15,
+//     name: "Sau Paulo",
+//     coordinates: [-58.3816, -34.6037],
+//   },
+// ];
 
-function App() {
-  return <div className="App"></div>;
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+export default function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ width: "1400px", borderStyle: "double" }}>
+        <ComposableMap data-tip="">
+          <ZoombleGroup zoom={1}>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} />
+                ))
+              }
+            </Geographies>
+          </ZoombleGroup>
+        </ComposableMap>
+      </div>
+    </div>
+  );
 }
-
-export default App;
